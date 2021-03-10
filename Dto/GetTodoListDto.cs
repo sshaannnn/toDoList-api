30e9da42c13a40c7c4627b9ecd788cf3c25f8226
sshaannnn <shan87826@gmail.com>
2021-03-10 12:52:47 +0800

    feat:add Dto

---
+++ Dto/GetTodoListDto.cs
@@ -0,0 +1,9 @@
+namespace toDoList_api.Dto
+{
+    public class GetTodoListDto
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public string State { get; set; }
+    }
+}
--- Services/TodoListService/TodoListService.cs
+++ Services/TodoListService/TodoListService.cs
@@ -1,15 +1,16 @@
 
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using toDoList_api.Dto;
 using toDoList_api.Models;
 
 namespace toDoList_api.Services.TodoListService
 {
     public class TodoListService : ITodoListService
     {
-        public async Task<ServiceResponse<List<TodoList>>> GetAllLists()
+        public async Task<ServiceResponse<List<GetTodoListDto>>> GetAllLists()
         {
-            ServiceResponse<List<TodoList>> serviceResponse = new ServiceResponse<List<TodoList>>();
+            // ServiceResponse<List<TodoList>> serviceResponse = new ServiceResponse<List<GetTodoListDto>>();
             throw new System.NotImplementedException();
         }
     }
